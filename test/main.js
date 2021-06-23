395b4deda6529ed237933b82f252e8c96d4a3d20
Rizky Haqiqi <rzkhqq@gmail.com>
2021-06-23 14:25:35 +0200

    #7 integrating storage and executor in test

--- test/main.js
+++ test/main.js
@@ -8,7 +8,7 @@ const { v4: uuidv4 } = require('uuid');
 describe('CORE', () => {
     describe('Executor', () => {
         describe('#runLocalFile', () => {
-            it('Fibo function should return 120 when the value is 5', async () => {
+            it('Fibo function should return 120 when the value is 5', async () => { 
                 const res = await Executor.runLocalFile('./etc/fibo.wasm', 'main', { i: 5 });
                 assert.strictEqual(res, 120);
             });
@@ -17,12 +17,13 @@ describe('CORE', () => {
 
     describe('Storage', () => {
         const storage = new Storage(uuidv4());
-        const path = './etc/fibo.wasm';
-        const objFibo = fs.readFileSync(path);
+        const path1 = './etc/fibo.wasm';
+        const path2 = './etc/helloworld.wasm';
+        const objFibo = fs.readFileSync(path1);
         let id = '';
         describe('#create', () => {
             it('Function will create a new entry to the catalog and save the file', async () => {
-                const filename = path.replace(/^.*[\\\/]/, '')
+                const filename = path1.replace(/^.*[\\\/]/, '')
                 id = storage.create(filename, objFibo);
                 assert.ok(id);
             });
@@ -35,13 +36,34 @@ describe('CORE', () => {
         });
         describe('#update', () => {
             it('Function will update the entry from the catalog and the file should be the updated', async () => {
-                const objHello = fs.readFileSync('./etc/helloworld.wasm');
+                const objHello = fs.readFileSync(path2);
                 storage.update(id, objHello);
 
                 const obj = storage.read(id);
                 assert.deepStrictEqual(obj, objHello);
                 assert.notDeepStrictEqual(obj, objFibo);
             });
+        });
+    });
+});
+
+describe('SERVER', () => {
+    const instanceId = 'f5e9de80-e9f2-4fcd-8ea0-2e089565ae9p';
+    let id = '';
+    describe('Execute a fibo function from storage', async () => {
+        it('Storage will store the source of the wasm file and will return the id of the file in the storage', async () => {
+            const storage = new Storage(instanceId);
+            const path1 = './etc/fibo.wasm';
+            const objFibo = fs.readFileSync(path1);
+
+            const filename = path1.replace(/^.*[\\\/]/, '')
+            id = storage.create(filename, objFibo);
+            assert.ok(id);
+        });
+        it('Executor will execute the fibo function with input 5, and gives return value of 120', async () => {
+            const executor = new Executor(instanceId);
+            const res = await executor.run(id, 'main', { i: 5 });
+            assert.strictEqual(res, 120);
         });
     });
 });
