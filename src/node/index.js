f9a0127528e6f0936261762d66121777af6e5e78
Rizky Haqiqi <rzkhqq@gmail.com>
2021-07-15 10:22:05 +0200

    #8 adapt the communicator

--- src/node/index.js
+++ src/node/index.js
@@ -1,5 +1,5 @@
 'use strict';
-const { default: Communicator } = require('./src/core/communicator');
+const { default: Communicator } = require('./communicator');
 // REMARK: InstanceId will be stored in the local storage, including port
 const instanceId = 'f5e9de80-e9f2-4fcd-8ea0-2e089565ae9p';
 const port = 3000;
