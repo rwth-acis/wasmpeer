e3fee4db6bd9f673948f7feccab0d4208ef968c7
Rizky Haqiqi <rzkhqq@gmail.com>
2021-09-17 19:38:03 +0200

    #12 working browser peer discovery
    node and browser client can find each other

--- test/browser/index.js
+++ test/browser/index.js
@@ -1,12 +1,3 @@
-// import 'regenerator-runtime/runtime';
-// import Communicator from '../../src/browser/communicator';
-
-// const urlMatcher = 'wasmpeer.org';
-
-// // REMARK: will be auto-generated in every new browser instance
-// const instanceId = 'f5e9de80-e9f2-4fcd-8ea0-2e089565ae9p';
-// window.wasmpeer = new Communicator(instanceId);
-
 import 'babel-polyfill';
 import Connector from '../../src/core/connector';
 
