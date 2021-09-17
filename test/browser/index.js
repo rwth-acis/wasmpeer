3c348a8f583265faaf535a7503138a81cc74ef5a
Rizky Haqiqi <rzkhqq@gmail.com>
2021-09-17 19:41:57 +0200

    #12 delete webrtc-explorer  parts

--- test/browser/index.js
+++ test/browser/index.js
@@ -1,3 +1,13 @@
+// import 'regenerator-runtime/runtime';
+// import Communicator from './communicator';
+
+// const urlMatcher = 'wasmpeer.org';
+
+// // REMARK: will be auto-generated in every new browser instance
+// const instanceId = 'f5e9de80-e9f2-4fcd-8ea0-2e089565ae9p';
+// window.wasmpeer = new Communicator(instanceId);
+
+
 import 'babel-polyfill';
 import Connector from '../../src/core/connector';
 
