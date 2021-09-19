7f0f8c7119e5a8162b87d82c663ff2ad948c7f3f
Rizky Haqiqi <rzkhqq@gmail.com>
2021-09-19 14:56:12 +0200

    #13 nodejs use reuseable storage component

--- src/node/accessor.js
+++ src/node/accessor.js
@@ -1,18 +1,23 @@
 'use strict';
 import fs from 'fs';
-
+import os from 'os';
 export default class Accessor {
-	constructor() {
+	constructor(instanceId) {
+
+		this.targetDir = os.tmpdir() + '/wasmpeer/storage/' + instanceId + '/';
+		if (!fs.existsSync(this.targetDir)) {
+			fs.mkdirSync(this.targetDir, { recursive: true });
+		};
 		// TODO: fomulate encryption/decryption key
 	}
 
 	put(path, object) {
 		const encrypted = this.encrypt(object);
-		return fs.writeFileSync(path, encrypted);
+		return fs.writeFileSync(this.targetDir + path, encrypted);
 	}
 
 	fetch(path) {
-		const raw = fs.readFileSync(path);
+		const raw = fs.readFileSync(this.targetDir + path);
 		return this.decrypt(raw);
 	}
 
--- src/node/index.js
+++ src/node/index.js
@@ -1,8 +1,77 @@
-'use strict';
-const { default: Communicator } = require('./communicator');
-// REMARK: InstanceId will be stored in the local storage, including port
-const instanceId = 'f5e9de80-e9f2-4fcd-8ea0-2e089565ae9p';
-const port = 3000;
+import Connector from '../core/connector.js';
+import Storage from "./storage/index.js";
+import Executor from "../core/executor.js";
+import Accessor from './accessor.js';
+import { v4 as uuidv4 } from 'uuid';
 
-const communicator = new Communicator(instanceId, port);
-communicator.start();
+
+export default class Wasmpeer {
+	constructor(instanceId, storage, connector, executor) {
+		this.instanceId = instanceId;
+		this.storage = storage;
+		this.connector = connector;
+		this.executor = executor;
+		this.connector.invoker = this.listen.bind(this);
+	}
+
+	static async build(instanceId, config) {
+
+		if (!instanceId) {
+			instanceId = uuidv4();
+		}
+		const accessor = new Accessor(instanceId);
+		const storage = await Storage.build(instanceId, accessor);
+
+		let peerId = await storage.read(instanceId).catch(_ => { });
+		peerId = peerId && peerId.id ? peerId : null;
+
+		const connector = await Connector.buildNodeJs(peerId, config);
+		if (!peerId) {
+			await storage.update(instanceId, JSON.stringify(connector.node.peerId.toJSON()));
+		}
+		const executor = new Executor(storage, connector);
+
+		return new Wasmpeer(instanceId, storage, connector, executor);
+	}
+
+	// invoke(targetPeerId, method, parameter) {
+	// 	return this.connector.call(targetPeerId, method, parameter);
+	// }
+
+	async listen(url) {
+	// 	const location = new URL(url);
+	// 	let [, serviceId, endpoint] = location.pathname.split('/');
+
+	// 	let params = {};
+	// 	if (location.search) {
+	// 		const search = location.search.substring(1);
+	// 		params = JSON.parse('{"' + decodeURI(search).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g, '":"') + '"}');
+	// 	}
+
+	// 	// REMARK: for testing purpose due to unavailability of upload mechanism
+	// 	serviceId = await this.setupForTest();
+	// 	const res = await this.execute(serviceId, endpoint, params);
+	// 	return res;
+	}
+
+	// async execute(serviceId, endpoint, params) {
+
+	// 	const res = await this.executor.run(serviceId, endpoint, params);
+	// 	return res;
+	// }
+
+	// async callOther(serviceId, endpoint, params) {
+	// 	const res = await this.connector.callFirst();
+	// 	return res;
+	// }
+
+	// async setupForTest() {
+	// 	const path1 = '/wasm/fibo.wasm';
+	// 	const objFibo = await fetch(path1).then(resp => resp.arrayBuffer());
+
+	// 	const filename = path1.replace(/^.*[\\\/]/, '')
+	// 	const id = await this.storage.storeService(filename, objFibo);
+
+	// 	return id;
+	// }
+}
