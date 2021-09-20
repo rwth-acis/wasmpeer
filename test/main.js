d90311d03e547645a539d232d8d3a56c831b3976
Rizky Haqiqi <rzkhqq@gmail.com>
2021-09-20 13:45:36 +0200

    #17 #3 #18 unit test adaptation

--- test/main.js
+++ test/main.js
@@ -5,37 +5,28 @@ import Storage from '../src/core/storage.js';
 import Accessor from '../src/node/accessor.js';
 import fs from 'fs';
 import { v4 as uuidv4 } from 'uuid';
+import Compiler from '../src/utils/compiler.js';
 
-describe('CORE', () => {
-    describe('Executor', () => {
-        describe('#runLocalFile', () => {
-            it('Fibo function should return 120 when the value is 5', async () => {
-                const objFibo = fs.readFileSync('./static/wasm/fibo.wasm');
-                const res = await Executor.runLocalFile(objFibo, 'main', { i: 5 });
-                assert.strictEqual(res, 120);
-            });
-        });
-    });
-
+describe('COMPONENTS', () => { 
     describe('Storage', () => {
         const instanceId = uuidv4();
         const accessor = new Accessor(instanceId);
         const storage = new Storage(accessor);
-        const path1 = './static/wasm/fibo.wasm';
+        const path1 = './static/wasm/main.wasm';
         const path2 = './static/wasm/helloworld.wasm';
-        const objFibo = fs.readFileSync(path1);
+        const objMain = fs.readFileSync(path1);
         let id = '';
         describe('#create', () => {
             it('Function will create a new entry to the catalog and save the file', async () => {
                 const filename = path1.replace(/^.*[\\\/]/, '')
-                id = await storage.storeService(filename, objFibo);
+                id = await storage.storeService(filename, objMain);
                 assert.ok(id);
             });
         });
         describe('#read', () => {
             it('Function will read the new entry from the catalog and the file should be the same', async () => {
                 const { source } = await storage.getService(id);
-                assert.deepStrictEqual(source, objFibo);
+                assert.deepStrictEqual(source, objMain);
             });
         });
         describe('#update', () => {
@@ -45,31 +36,101 @@ describe('CORE', () => {
 
                 const { source } = await storage.getService(id);
                 assert.deepStrictEqual(source, objHello);
-                assert.notDeepStrictEqual(source, objFibo);
+                assert.notDeepStrictEqual(source, objMain);
             });
         });
     });
 });
 
-describe('SERVER', () => {
+describe('SERVICES', () => {
     let id = '';
     const instanceId = uuidv4();
     const accessor = new Accessor(instanceId);
     const storage = new Storage(accessor);
-    describe('Execute a fibo function from storage', async () => {
-        it('Storage will store the source of the wasm file and will return the id of the file in the storage', async () => {
-            
-            const path1 = './static/wasm/fibo.wasm';
-            const objFibo = fs.readFileSync(path1);
 
+    describe('Calculator service', async () => {
+        let service = null;
+        const path1 = './static/as/calculator.ts';
+        it('Compile the assembly script', async () => {
+            const mainRaw = fs.readFileSync(path1);
+            service = await Compiler.AS(mainRaw.toString());
+            assert.ok(service);
+        });
+
+        it('Compiled module has generated correct descriptor', async () => {
+            assert.deepEqual(service.meta, {
+                add: { name: 'add', paramsType: { x: 'i32', y: 'i32'}, returnType: 'i32' },
+                subtract: { name: 'subtract', paramsType: { x: 'i32', y: 'i32'}, returnType: 'i32' },
+                multiple: { name: 'multiple', paramsType: { x: 'i32', y: 'i32'}, returnType: 'i32' },
+                divide: { name: 'divide', paramsType: { x: 'i32', y: 'i32'}, returnType: 'i32' },
+                fib: { name: 'fib', paramsType: { n: 'i32' }, returnType: 'i32' }
+            });
+        });
+
+        it('Store the service to storage', async () => {
             const filename = path1.replace(/^.*[\\\/]/, '')
-            id = await storage.storeService(filename, objFibo);
+            id = await storage.storeService(filename, service.source, service.raw, service.meta);
             assert.ok(id);
         });
-        it('Executor will execute the fibo function with input 5, and gives return value of 120', async () => {
+
+        describe('Execute the service', () => {
             const executor = new Executor(storage);
-            const res = await executor.run(id, 'main', { i: 5 });
-            assert.strictEqual(res, 120);
+
+            it('Add 5 and 6 returns 11', async () => {
+                const res = await executor.run(id, 'add', { x: 5, y: 6 });
+                assert.strictEqual(res, 11);
+            });
+    
+            it('Subtract 5 and 6 returns -1', async () => {
+                const res = await executor.run(id, 'subtract', { x: 5, y: 6 });
+                assert.strictEqual(res, -1);
+            });
+    
+            it('Multiple 5 and 6 returns 30', async () => {
+                const res = await executor.run(id, 'multiple', { x: 5, y: 6 });
+                assert.strictEqual(res, 30);
+            });
+    
+            it('Divide 30 and 6 returns 5', async () => {
+                const res = await executor.run(id, 'divide', { x: 30, y: 6 });
+                assert.strictEqual(res, 5);
+            });
+    
+            it('Fibonacci of 5 returns 8', async () => {
+                const res = await executor.run(id, 'fib', { x: 5 });
+                assert.strictEqual(res, 8);
+            });
+        });
+    });
+
+    describe('String service', async () => {
+        let service = null;
+        const path1 = './static/as/string.ts';
+        it('Compile the assembly script', async () => {
+            const mainRaw = fs.readFileSync(path1);
+            service = await Compiler.AS(mainRaw.toString());
+            assert.ok(service);
+        });
+
+        it('Compiled module has generated correct descriptor', async () => {
+            assert.deepEqual(service.meta, {
+                concat: { name: 'concat', paramsType: { a: 'usize', b: 'usize'}, returnType: 'usize' }
+            });
+        });
+
+        it('Store the service to storage', async () => {
+            const filename = path1.replace(/^.*[\\\/]/, '')
+            id = await storage.storeService(filename, service.source, service.raw, service.meta);
+            assert.ok(id);
+        });
+
+        
+        describe('Execute the service', () => {
+            const executor = new Executor(storage);
+            it('Concat "hello " and "world" returns "hello world"', async () => {
+                const res = await executor.run(id, 'concat', { a: 'hello ', b: 'world' });
+                assert.strictEqual(res, 'hello world');
+            });
         });
     });
 });
