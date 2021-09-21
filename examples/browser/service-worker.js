e69702af3c321c7be7c1bd2ae4c83259f3b89760
Rizky Haqiqi <rzkhqq@gmail.com>
2021-09-21 09:18:01 +0200

    #21 change indentation of the code to 4

--- examples/browser/service-worker.js
+++ examples/browser/service-worker.js
@@ -8,20 +8,20 @@ const instanceId = 'f5e9de80-e9f2-4fcd-8ea0-2e089565ae9p';
 
 // The install handler
 self.addEventListener('install', event => {
-  event.waitUntil(self.skipWaiting());
+	event.waitUntil(self.skipWaiting());
 });
 
 // The activate handler
 self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
+	event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener('fetch', event => {  
-  const target = event.request.url.toString();
-  const communicator = new Communicator(instanceId);
-  // reroute every call that match the matcher
-  if (target.includes(urlMatcher)) {
-    event.respondWith(communicator.listen(target));
-  }
+self.addEventListener('fetch', event => {
+	const target = event.request.url.toString();
+	const communicator = new Communicator(instanceId);
+	// reroute every call that match the matcher
+	if (target.includes(urlMatcher)) {
+		event.respondWith(communicator.listen(target));
+	}
 });
 
--- src/core/connector.js
+++ src/core/connector.js
@@ -10,216 +10,216 @@ import PeerId from 'peer-id';
 import { v4 as uuidv4 } from 'uuid';
 
 export default class Connector {
-  constructor(node, log) {
-    this.node = node;
-    this.log = log;
-    this.callStack = {};
-    this.invoker = () => {};
+	constructor(node, log) {
+		this.node = node;
+		this.log = log;
+		this.callStack = {};
+		this.invoker = () => { };
 
-    node.handle(node.peerId.toB58String(), async ({ connection, stream }) => {
-      try {
-        await pipe(
-          stream,
-          async (source) => {
-            for await (const message of source) {
-              const req = JSON.parse(String(message));
+		node.handle(node.peerId.toB58String(), async ({ connection, stream }) => {
+			try {
+				await pipe(
+					stream,
+					async (source) => {
+						for await (const message of source) {
+							const req = JSON.parse(String(message));
 
-              if (req.type === 'ANS') {
-                if (!this.callStack[req.callId]) {
-                  throw new Error('Incoming not recognized: ', req.callId);
-                }
-                this.callStack[req.callId](req);
-                this.callStack[req.callId] = null;
-              } else {
-                // UI.log('INCOMING REQ: ', req.callId);
-                const resp = await this.invoker('https://wasmpeer.org/e21af0b5-2a5c-4e93-9ee2-d3449fcf23e3/main?i=10');
+							if (req.type === 'ANS') {
+								if (!this.callStack[req.callId]) {
+									throw new Error('Incoming not recognized: ', req.callId);
+								}
+								this.callStack[req.callId](req);
+								this.callStack[req.callId] = null;
+							} else {
+								// UI.log('INCOMING REQ: ', req.callId);
+								const resp = await this.invoker('https://wasmpeer.org/e21af0b5-2a5c-4e93-9ee2-d3449fcf23e3/main?i=10');
 
-                const payload = {
-                  type: 'ANS',
-                  req: req,
-                  callId: req.callId,
-                  data: resp
-                };
+								const payload = {
+									type: 'ANS',
+									req: req,
+									callId: req.callId,
+									data: resp
+								};
 
-                try {        
-                  const { stream } = await connection.newStream([connection.remotePeer.toB58String()])
-                  await Invocator.send(JSON.stringify(payload), stream);
-                  return;
-                } catch (err) {
-                  console.error('Could not negotiate chat protocol stream with peer', err)
-                }
-              }
-            }
-          }
-        )
-  
-        await pipe([], stream)
-      } catch (err) {
-        console.error(err)
-      }
-    });
-  }
+								try {
+									const { stream } = await connection.newStream([connection.remotePeer.toB58String()])
+									await Invocator.send(JSON.stringify(payload), stream);
+									return;
+								} catch (err) {
+									console.error('Could not negotiate chat protocol stream with peer', err)
+								}
+							}
+						}
+					}
+				)
 
-  static buildNodeJs(peerId, params = {}) {
-    params.transport = {
-      [WebRTCStar.prototype[Symbol.toStringTag]]: {
-        wrtc
-      }
-    }
-    return Connector.build(peerId, params);
-  }
+				await pipe([], stream)
+			} catch (err) {
+				console.error(err)
+			}
+		});
+	}
 
-  static async build(peerId, params = {}) {
-    let { transport, log, sigServers, bootstrappers } = params;
-    
-    transport = transport || {};
-    log = log || (() => {});
-    peerId = peerId ? (await PeerId.createFromJSON(peerId)) : await PeerId.create();
+	static buildNodeJs(peerId, params = {}) {
+		params.transport = {
+			[WebRTCStar.prototype[Symbol.toStringTag]]: {
+				wrtc
+			}
+		}
+		return Connector.build(peerId, params);
+	}
 
-    const node = await Libp2p.create({
-      peerId: peerId,
-      addresses: {
-        listen: sigServers
-      },
-      modules: {
-        transport: [Websockets, WebRTCStar],
-        streamMuxer: [Mplex],
-        connEncryption: [NOISE],
-        peerDiscovery: [Bootstrap]
-      },
-      config: {
-        peerDiscovery: {
-          autoDial: false,
-          [Bootstrap.tag]: {
-            enabled: true,
-            list: bootstrappers
-          }
-        },
-        transport: transport
-      },
-    });
+	static async build(peerId, params = {}) {
+		let { transport, log, sigServers, bootstrappers } = params;
 
-    node.connectionManager.on('peer:connect', (connection) => {
-      log(`Connected to ${connection.remotePeer.toB58String()}!`)
-    });
+		transport = transport || {};
+		log = log || (() => { });
+		peerId = peerId ? (await PeerId.createFromJSON(peerId)) : await PeerId.create();
 
-    node.connectionManager.on('peer:disconnect', (connection) => {
-      log(`Disconnected from '${connection.remotePeer.toB58String()}`)
-    });
-  
-    // node.on('peer:discovery', (peerId) => {
-    //   log(`Found peer ${peerId.toB58String()}`)
-    // })
+		const node = await Libp2p.create({
+			peerId: peerId,
+			addresses: {
+				listen: sigServers
+			},
+			modules: {
+				transport: [Websockets, WebRTCStar],
+				streamMuxer: [Mplex],
+				connEncryption: [NOISE],
+				peerDiscovery: [Bootstrap]
+			},
+			config: {
+				peerDiscovery: {
+					autoDial: false,
+					[Bootstrap.tag]: {
+						enabled: true,
+						list: bootstrappers
+					}
+				},
+				transport: transport
+			},
+		});
 
-    await node.start();
-    log('peerId: ' + node.peerId.toB58String());
+		node.connectionManager.on('peer:connect', (connection) => {
+			log(`Connected to ${connection.remotePeer.toB58String()}!`)
+		});
 
-    return new Connector(node, log);
-  }
+		node.connectionManager.on('peer:disconnect', (connection) => {
+			log(`Disconnected from '${connection.remotePeer.toB58String()}`)
+		});
 
-  async sendMessage(targetPeerId, message) {
-    
-    this.node.peerStore.peers.forEach(async peerData => {
-      const peerId = peerData.id.toB58String();
-      if (targetPeerId !== peerId) return;
+		// node.on('peer:discovery', (peerId) => {
+		//   log(`Found peer ${peerId.toB58String()}`)
+		// })
 
-      let connection = this.node.connectionManager.get(peerData.id)
-      if (!connection) {
-        connection = await this.node.dial(peerData.id);
-      }
+		await node.start();
+		log('peerId: ' + node.peerId.toB58String());
 
-      if (!connection) return;
+		return new Connector(node, log);
+	}
 
-      try {
-        const { stream } = await connection.newStream([peerId])
-        await Invocator.send(message, stream);
-      } catch (err) {
-        console.error('Could not negotiate chat protocol stream with peer', err)
-      }
-    })
-  }
+	async sendMessage(targetPeerId, message) {
 
-  async invoke(targetPeerId, payload) {
-    this.node.peerStore.peers.forEach(async peerData => {
-      const peerId = peerData.id.toB58String();
-      if (targetPeerId !== peerId) return;
+		this.node.peerStore.peers.forEach(async peerData => {
+			const peerId = peerData.id.toB58String();
+			if (targetPeerId !== peerId) return;
 
-      let connection = this.node.connectionManager.get(peerData.id)
-      if (!connection) {
-        connection = await this.node.dial(peerData.id);
-      }
-      if (!connection) return;
+			let connection = this.node.connectionManager.get(peerData.id)
+			if (!connection) {
+				connection = await this.node.dial(peerData.id);
+			}
 
-      try {        
-        const { stream } = await connection.newStream([peerId])
-        await Invocator.send(JSON.stringify(payload), stream);
-        return;
-      } catch (err) {
-        console.error('Could not negotiate chat protocol stream with peer', err)
-      }
-    })
-  }
+			if (!connection) return;
 
-  async call(targetPeerId, method, parameter) {
-    const payload = {
-      callId: uuidv4(),
-      method: method,
-      type: 'GET',
-      serviceId: 'abcdef',
-      parameter: parameter
-    };
+			try {
+				const { stream } = await connection.newStream([peerId])
+				await Invocator.send(message, stream);
+			} catch (err) {
+				console.error('Could not negotiate chat protocol stream with peer', err)
+			}
+		})
+	}
 
-    const ress = new Promise((resolve, reject) => {
-      this.invoke(targetPeerId, payload);
-      this.callStack[payload.callId] = resolve;
-    });
+	async invoke(targetPeerId, payload) {
+		this.node.peerStore.peers.forEach(async peerData => {
+			const peerId = peerData.id.toB58String();
+			if (targetPeerId !== peerId) return;
 
-    const resp = await ress.then(x => {
-      return x.data;
-    });
+			let connection = this.node.connectionManager.get(peerData.id)
+			if (!connection) {
+				connection = await this.node.dial(peerData.id);
+			}
+			if (!connection) return;
 
-    return resp;
-  }
+			try {
+				const { stream } = await connection.newStream([peerId])
+				await Invocator.send(JSON.stringify(payload), stream);
+				return;
+			} catch (err) {
+				console.error('Could not negotiate chat protocol stream with peer', err)
+			}
+		})
+	}
 
-  // callFirst() {
-  //   const list = this.peer.fingerTable.getTable();
-  //   const a = Object.values(list)[0];
+	async call(targetPeerId, method, parameter) {
+		const payload = {
+			callId: uuidv4(),
+			method: method,
+			type: 'GET',
+			serviceId: 'abcdef',
+			parameter: parameter
+		};
 
-  //   return this.call(a.fingerId, null);
-  // }
+		const ress = new Promise((resolve, reject) => {
+			this.invoke(targetPeerId, payload);
+			this.callStack[payload.callId] = resolve;
+		});
+
+		const resp = await ress.then(x => {
+			return x.data;
+		});
+
+		return resp;
+	}
+
+	// callFirst() {
+	//   const list = this.peer.fingerTable.getTable();
+	//   const a = Object.values(list)[0];
+
+	//   return this.call(a.fingerId, null);
+	// }
 }
 
 const Invocator = {
-  handler: async ({ connection, stream }) => {
-    try {
-      await pipe(
-        stream,
-        async function (source) {
-          for await (const message of source) {
-            
-            console.info(`${connection.remotePeer.toB58String().slice(0, 8)}: ${String(message)}`)
-          }
-        }
-      )
+	handler: async ({ connection, stream }) => {
+		try {
+			await pipe(
+				stream,
+				async function (source) {
+					for await (const message of source) {
 
-      await pipe([], stream)
-    } catch (err) {
-      console.error(err)
-    }
-  },
-  send: async (message, stream) => {
-    try {
-      await pipe(
-        [message],
-        stream,
-        async (source) => {
-          for await (const message of source) {
-            console.info(String(message))
-          }
-        }
-      )
-    } catch (err) {
-      console.error(err)
-    }
-  }
+						console.info(`${connection.remotePeer.toB58String().slice(0, 8)}: ${String(message)}`)
+					}
+				}
+			)
+
+			await pipe([], stream)
+		} catch (err) {
+			console.error(err)
+		}
+	},
+	send: async (message, stream) => {
+		try {
+			await pipe(
+				[message],
+				stream,
+				async (source) => {
+					for await (const message of source) {
+						console.info(String(message))
+					}
+				}
+			)
+		} catch (err) {
+			console.error(err)
+		}
+	}
 }
